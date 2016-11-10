67af7e09c1ae3cf44d9fdb0333a1a14eeeef0e26
Mazin Zakaria <mazinzakaria1@gmail.com>
2016-11-10 15:17:26 -0500

    Allow custom username for superusers

--- users/models.py
+++ users/models.py
@@ -9,9 +9,6 @@ from string import ascii_lowercase, digits
 
 class AccountManager(BaseUserManager):
     def create_user(self, password=None, **kwargs):
-        if not kwargs.get('username'):
-            raise ValueError('Users must have a username.')
-        
         #account = self.model(username=kwargs.get('username'))
         account = self.model(username=self.gen_random_username())
         account.set_password(password)
@@ -19,9 +16,14 @@ class AccountManager(BaseUserManager):
         return account
     
     def create_superuser(self, password, **kwargs):
-        account = self.create_user(password=None, **kwargs)
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a username.')
+        #account = self.create_user(password=None, **kwargs)
+        account = self.model(username=kwargs.get('username'))
+        account.set_password(password)
         account.is_admin = True
         account.save()
+
         return account
         
     def gen_random_username(self):        
@@ -47,7 +49,23 @@ class Account(AbstractBaseUser):
     
     USERNAME_FIELD = 'username'
     
+    def is_staff(self):
+        return self.is_admin
+
     def __unicode__(self):
         return self.username
-   
 
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def is_superuser(self, app_label):
+        return self.is_superuser
+   
+    def get_short_name(self):
+        return self.username 
